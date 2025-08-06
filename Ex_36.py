20e8879ee711d5d472efec65e3772d09cb19ceec
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2025-08-06 18:59:23 -0300

    Exercicio sobre Singleton | POO

---
+++ Ex_36.py
@@ -0,0 +1,26 @@
+class DatabaseConnection:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls)
+            cls._instance.logs = []
+        return cls._instance
+
+    def __init__(self):
+        self.status = 'desconnected'
+
+    def connect(self):
+        if self.status == 'desconnected':
+            print('Connected to database')
+            self.status = 'Connected'
+
+
+db1 = DatabaseConnection()
+db2 = DatabaseConnection()
+
+db1.connect()
+db2.connect()
+
+print(db1 is db2)  # True (mesma instância)
+print(db1.status)  # connected
+print(db2.status)  # connected
